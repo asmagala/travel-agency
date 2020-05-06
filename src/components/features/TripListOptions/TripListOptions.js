7b8fe5a21e57917c4ae064f4547a3b1c712d2747
Arkadiusz Smagała <asmagala@outlook.com>
2020-05-06 22:15:02 +0200

    Add reducers for filters.

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -7,17 +7,20 @@ import {Row, Col} from 'react-flexbox-grid';
 class TripListOptions extends React.Component {
   handleTags(tag, checked){
     if(checked) {
-      console.log('Adding tag', tag);
+      console.log('Adding tag', tag, checked);
       // TODO - use action dispatcher from props
+      this.props.addTag(tag);
     } else {
-      console.log('Removing tag', tag);
+      console.log('Removing tag', tag, checked);
       // TODO - use action dispatcher from props
+      this.props.removeTag(tag);
     }
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    this.props.changeDuration({type, value});
   }
 
   handleSearch(phrase){
@@ -73,6 +76,9 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  addTag: PropTypes.func,
+  removeTag: PropTypes.func,
+  changeDuration: PropTypes.func,
 };
 
 export default TripListOptions;
